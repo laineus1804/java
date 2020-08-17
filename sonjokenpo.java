ff3cbd5cefb893cc774a0904b047484463d892a2
laineus1804 <59533157+laineus1804@users.noreply.github.com>
2020-08-16 23:07:20 -0300

    Add files via upload

---
+++ sonjokenpo.java
@@ -0,0 +1,60 @@
+package courjava;
+
+
+
+	import java.util.Random;
+	import java.util.Scanner;
+
+	
+	public class sonjokenpo {
+	public static void main(String[] args) {
+			//1-papel, 2-pedra; 3-Tesoura.
+		//objetos
+			Scanner leitor = new Scanner(System.in);
+			Random gerador =new Random();
+			System.out.println("=====jokenpo======");
+			System.out.println("joueur:");
+			
+			
+			int nUser;
+			int esComputer;
+			int vUser = 0;
+			int vComputer = 0;
+			
+			
+			nUser = leitor.nextInt();
+			esComputer = gerador.nextInt(3) +1;
+			
+			
+			//processamente 
+			switch(esComputer) {
+			case 1:
+				System.out.println("Computer choisie papel");
+				break;
+			case 2:
+				System.out.println("Computer choisie pedra");
+				break;
+			case 3:
+				System.out.println("Computer choisie tesoura");
+				break;
+			
+			default: 
+	            System.out.println ("Ruim");
+		} 
+			
+			if(nUser == esComputer) {
+				System.out.println("Empate.");
+			}else if((nUser - esComputer) == -1 ||
+					(nUser - esComputer) == 2) {
+				System.out.println("User vencedor.");
+				
+			}
+			else{
+				System.out.println("Computer vencedor");
+			
+			  }
+		
+			
+	    }
+
+	}

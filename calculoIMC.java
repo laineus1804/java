9a1cc3d4af285174b18b24d15adedddbf5585553
laineus1804 <59533157+laineus1804@users.noreply.github.com>
2020-08-17 20:51:46 -0300

    Update calculoIMC.java

--- calculoIMC.java
+++ calculoIMC.java
@@ -3,14 +3,14 @@ package courjava;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-import Contas.…tat;
+import Contas.√âtat;
 
 public class calculoIMC {
 
 	public static void main(String[] args) {
 		
 		
-		//Vari·veis
+		//Vari√°veis
 		
 		double peso,altura,imc;
 		
@@ -18,30 +18,42 @@ public class calculoIMC {
 		Scanner teclado = new Scanner(System.in);
 		DecimalFormat formatador = new DecimalFormat("#0.00");
 		//entrada
-		System.out.println("c·lculo do IMC");
+		System.out.println("c√°lculo do IMC");
 		System.out.print("Digite o seu peso em kilogramas: ");
+		
 		peso = teclado.nextDouble();
 		System.out.print("Digite o sua altura em metros: ");
+		
 		altura = teclado.nextDouble();
 		imc = peso / (altura*altura);
 		//saida
 		System.out.println("Imc: " + formatador.format(imc));
 		
 		 if (imc < 16.99 ){
-			System.out.println("insuficiÍncia ponder·vel");
+			System.out.println("insufici√™ncia ponder√°vel");
 			
 		}else if (imc >= 16.99 && imc <= 18.5) {
 			System.out.println("Abaixo do peso");
-		}else if (imc >= 18.5 && imc <= 24.99) {
+		}
+		
+		else if (imc >= 18.5 && imc <= 24.99) {
 			System.out.println("Normale");
-		}else if (imc >=25 && imc <= 29.99) {
+		}
+		
+		else if (imc >=25 && imc <= 29.99) {
 			System.out.println("En surpoids");
-		}else if (imc >=30 && imc <=34.99 ) {
-			System.out.println("AbÈsitÈ I");
-		}else if (imc >=35 && imc <=39.99) {
-			System.out.println("AbÈsitÈ II(SÈvËre)");
-		}else if (imc >= 40) {
-			System.out.println("AbÈsitÈ III(morbide)");
+		}
+		
+		else if (imc >=30 && imc <=34.99 ) {
+			System.out.println("Ab√©sit√© I");
+		}
+		
+		else if (imc >=35 && imc <=39.99) {
+			System.out.println("Ab√©sit√© II(S√©v√®re)");
+		}
+		
+		else if (imc >= 40) {
+			System.out.println("Ab√©sit√© III(morbide)");
 		}
 		 
 		

609020a931eb40899bd18c98a1e2640d45901cf8
paul gaslet <lainepaul199@gmail.com>
2020-10-12 13:21:11 -0300

    archiver

---
+++ fondamental-java/POO/Enxada.java
@@ -0,0 +1,20 @@
+package minecraft;
+
+public class Enxada extends Bloco {
+    //atributos
+	boolean conquista;
+	//metros
+	void arar() {
+		System.out.println("Terre preparada para o plantio");
+		conquista = true; 
+		
+	}
+	
+		//polimorfismo
+		void minerar() {
+			System.out.println("Dano atribuído");
+		}
+	
+		
+	}
+
